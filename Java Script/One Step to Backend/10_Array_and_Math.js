c6f1f34a9fb4b011af2a3fa9d6503b1f0e71a3e7
Assassin318554 <nahinsikdar123@gmail.com>
2024-01-25 01:41:14 +0600

    One Step to Backend

---
+++ Java Script/One Step to Backend/10_Array_and_Math.js
@@ -0,0 +1,24 @@
+const arr = [-10, 3, 4, 5, 60, -1];
+// arr.sort(); // Sort Like lexicographycally order Like 50000 dileo 5 er por ebong 6 er aage ashbe
+// console.log(arr);
+
+// So we have to sort an array in javascript like this
+// arr.sort (function(a, b) {
+//     return a - b;
+// });
+
+// Or, 
+arr.sort ((a, b) => a - b);
+
+console.log(arr);
+// console.log(arr[arr.length - 1]);
+
+// function minArray(arr){
+    //     return Math.min()
+// }
+
+// const str = "nahin";
+// str.sort();
+// console.log(str);
+
+
