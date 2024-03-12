1c2afabee2bec82f6f681c770d2a91c6f7283dfd
Assassin318554 <nahinsikdar123@gmail.com>
2024-03-12 07:15:57 +0600

    a

---
+++ Java Script/Apna College/Lecture 12 Callbacks, Promises & Async Await/callbacks.js
@@ -0,0 +1,16 @@
+//function hello() {
+//  console.log('hello nahin');
+//}
+
+//setTimeout(hello, 2000); // 2s = 2000
+
+console.log('one');
+console.log('two');
+
+//setTimeout(() => {
+console.log('How is going?');
+//}, 4000);
+
+console.log('three'); // This line will not wait
+console.log('four'); // This line will not wait
+
---
+++ Java Script/Apna College/Lecture 12 Callbacks, Promises & Async Await/callbacks2.js
@@ -0,0 +1,12 @@
+// call backs in syncronusly:
+function sum(a, b) {
+    console.log(`Sum is: ${a + b}`);
+}
+// function calculator(a, b, sumCallBack) {
+//     sumCallBack(a, b);
+// }  
+// calculator(1, 2, sum);
+  
+
+// call backs in asyncronusly:
+setTimeout(sum(1,2), 2000);
