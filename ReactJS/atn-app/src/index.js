1c4270acb667becca16e40f7ee9ae717b99d9fd6
Assassin318554 <nahinsikdar123@gmail.com>
2024-03-19 23:38:12 +0600

    a

---
+++ ReactJS/atn-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Tanvir from './tanvir';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    Hello
+    < Tanvir />
+  </>
+);
---
+++ ReactJS/atn-app/src/tanvir.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Ador() {
+  return (<>Hello Ador`@G</>);
+}
+
+function Tanvir() {
+  return (<>Hello Ador</>);
+}
+
+
+export default Tanvir;
