37b222bd7ebcd8dab9cd11a0c1e0edbc7cd200ec
Assassin318554 <nahinsikdar123@gmail.com>
2024-04-21 16:06:20 +0600

    a

--- Java Script/Apna College/Lecture 12 Callbacks, Promises & Async Await/callbacks2.js
+++ Java Script/Apna College/Lecture 12 Callbacks, Promises & Async Await/callbacks2.js
@@ -10,3 +10,27 @@
 
 // call backs in asyncronusly:
 // setTimeout(sum(1,2), 2000);
+
+
+
+var i = 1;
+function getData(dataId, getNextData) {
+    setTimeout(() => { // 2s
+        console.log(`Data ${i++}: ${dataId}`);
+        if(getNextData){
+            getNextData();
+        }
+    }, 2000);
+}
+
+console.log("Getting data 1...");
+getData(14156, () => {
+    console.log("Getting data 2...");
+    getData(24685, () => {
+        console.log("Getting data 3...");
+        getData(37486, () => {
+            console.log("Getting data 4...");
+            getData(44685);
+        });
+    });
+});
